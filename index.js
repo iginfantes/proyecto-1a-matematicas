2a62e5a267cf46825dfed5cd8665fb471bdd81ec
iginfantes <iginfantes@minsait.com>
2022-06-19 11:10:59 +0200

    primera version modulo mates básicas

---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports={
+    /**
+     * Suma de dos números
+     * @example
+     * num1= 3, num2=4 => resultado 7
+     * @param {*} num1 Numero 1 de la suma 
+     * @param {*} num2 Numero 2 de la suma 
+     * @returns 
+     */
+    suma: (num1,num2)=>{
+        return num1+num2;
+    },
+
+     /**
+     * Multiplicación de dos números
+     * @example
+     * num1= 3, num2=4 => resultado 12
+     * @param {*} num1 Numero 1 de la multiplicación
+     * @param {*} num2 Numero 2 de la multiplicación 
+     * @returns 
+     */
+    multiplicar: (num1,num2)=>{
+        return num1*num2;
+    },
+    /**
+     * Division de dos números
+     * @example
+     * num1= 12, num2=3 => resultado 4
+     * @param {*} num1 Numero 1 de la división
+     * @param {*} num2 Numero 2 de la división 
+     * @returns 
+     */
+    dividir: (num1,num2)=>{
+        return num1/num2;
+    }
+}
